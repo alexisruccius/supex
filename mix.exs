f39e399334155e342fe320a3f0e38afdf2ec8234
Alexis Ruccius <a.ruccius@posteo.de>
2025-08-03 18:36:15 +0200

    Add Credo

--- mix.exs
+++ mix.exs
@@ -31,7 +31,8 @@ defmodule Supex.MixProject do
   defp deps do
     [
       {:ex_doc, "~> 0.34", only: :dev, runtime: false},
-      {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false}
+      {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false}
     ]
   end
 
